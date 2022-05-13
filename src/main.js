cab1d4fadc130385ef48fe5d051700c736363ff3
zengzhiyuan <2916363651@qq.com>
2022-05-13 17:57:09 +0800

    完成了登录功能

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import './plugins/element.js'
+//导入字体图标
+import './assets/font/iconfont.css'
+// 导入全局样式
+import './assets/css/global.css'
+import axios from 'axios'
 
-Vue.config.productionTip = false
+//配置请求的根路径
+axios.defaults.baseURL = 'http://zhuchaonan.myuxi.top/api/private/v1/';
+Vue.prototype.$http = axios;
+Vue.config.productionTip = false;
 
 new Vue({
   router,
